f5f21d89d18a92b40930ad6563d46ee01c83f070
Satyajeet Behera <jagajajpur3@gmail.com>
2023-10-08 22:46:42 +0530

    Update Technologies.jsx

--- src/Pages/Technologies.jsx
+++ src/Pages/Technologies.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { techStackDetails } from "../Details";
+import { FaJava, FaHtml5 } from "react-icons/fa6";
+import { SiJavascript } from "react-icons/si";
 
 function Technologies() {
   const {
