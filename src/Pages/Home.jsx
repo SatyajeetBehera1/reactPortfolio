15788c73ec7505cb2c183caf48318d307cbc221f
Satyajeet Behera <jagajajpur3@gmail.com>
2023-12-14 09:16:49 +0530

    change

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -106,7 +106,7 @@ function Home() {
                
         </h2>
 
-        <a href='https://drive.google.com/file/d/1Du91dkFeFgSJKhHDosUIZxicT6TUV8-0/view?usp=drive_link' target='_blank' >
+        <a href='https://drive.google.com/file/d/1Du91dkFeFgSJKhHDosUIZxicT6TUV8-0/view?usp=drive_link' target='_blank' rel="noreferrer">
     <button type="button" class="text-white bg-gradient-to-br from-blue-500 to-purple-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-5
     " ref={butt}>Resume</button></a>
 
