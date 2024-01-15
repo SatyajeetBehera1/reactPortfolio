0dac10c46040c89b64c565450bb8f98f9fdeb28a
Satyajeet Behera <jagajajpur3@gmail.com>
2024-01-15 21:10:05 +0530

    Update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -8,7 +8,7 @@ function Projects() {
       <section >
         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
           Projects
-        </h1>
+        </h1> 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10">
           {React.Children.toArray(
             projectDetails.map(
