2415aff7319dc0e3e3b01d58471102c59205cb43
Satyajeet Behera <jagajajpur3@gmail.com>
2024-07-11 14:11:45 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -107,7 +107,7 @@ function Home() {
         </h2>
 
         <a href='https://drive.google.com/file/d/1E3e8LFZRIrt3i66M7V8AW-Qp7UDNZx0P/view?usp=drive_link' target='_blank' rel="noreferrer">
-    <button type="button" class="text-white bg-gradient-to-br from-blue-500 to-purple-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-5
+    <button type="button" class="text-white bg-gradient-to-r from-indigo-600 to-pink-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-5
     " ref={butt}>Resume</button></a>
 
       </div>
