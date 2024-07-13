e89f8dab72439743bd5e1a1bc037fa7fc586afe4
Satyajeet Behera <jagajajpur3@gmail.com>
2024-07-13 15:23:26 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -89,6 +89,7 @@ function Home() {
           ref={h13}
           className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
         >
+          <br></br>
           {tagline}
           <TypeAnimation
             sequence={[
